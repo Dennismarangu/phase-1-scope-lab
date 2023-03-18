f43a5a760316da43b8b91f36b84cf046b4fd4174
+ <+>
2023-03-18 10:13:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+customerName
+
+
+// function upperCaseCustomerName() {
+//      return customerName.toUpperCase();
+//  }
+//  const newName = upperCaseCustomerName();
+//  newName
+
+//  function setBestCustomer() {
+//      return 'not bob';
+//  }
+//  const bestCustomer = setBestCustomer();
+// bestCustomer
+
+function upperCaseCustomerName() {
+    newName = customerName.toUpperCase();
+    return newName;
+  }
+  console.log(upperCaseCustomerName());
+
+function overwriteBestCustomer() {
+    bestCustomer = "Jane";
+    return bestCustomer;
+}
+console.log(overwriteBestCustomer());
+
+const leastFavoriteCustomer = "Mary";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Hug";
+}
+console.log(changeLeastFavoriteCustomer());
+
+
+
+
